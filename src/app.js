78c70c1ff78d81f8d646e4cdb553ea2add4e63cd
Sujith Reddy <sujithdnd@gmail.com>
2026-07-13 12:51:49 +0530

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const applicationRoutes=require("./routes/applicationRoutes");
 const authRoutes = require("./routes/authRoutes");
 const aiRoutes = require("./routes/aiRoutes"); 
 const resumeRoutes = require("./routes/resumeRoutes");
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
+
+app.use(express.json());
 
 app.use(express.json());
 
--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-const PORT = 5000||process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-// console.log("hii");
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
